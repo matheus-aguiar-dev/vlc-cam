dccabb9661adfd1d2abc5d38571cc4cf69910e8b
matheus.aguiar <matheus.aguiar@policorp.com.br>
2023-08-23 14:37:05 -0400

    added record and snapshot

--- src/start.c
+++ src/start.c
@@ -15,14 +15,13 @@ GtkWidget *window,*player_widget;
 GtkBuilder *builder;
 struct CamInfo cam;
 const char* const vlc_args[] = {
-	"--v4l2-chroma=YUYV",
+	"--no-xlib",
+	"--v4l2-chroma=mjpg",
 	"--v4l2-width=800",
 	"--v4l2-height=600",
 	"--video-filter=transform",
 	"--transform-type=hflip",
 };
-
-void player_widget_on_realize(GtkWidget *widget, gpointer data);
 
 struct CamInfo{
 	char name[100];
@@ -30,32 +29,37 @@ struct CamInfo{
 	size_t size;
 };
 
-void configure_callback(GtkWidget *widget, GdkEvent *event, gpointer data){
-	//gtk_widget_set_size_request(player_widget, cam.sizes[0][0], cam.sizes[0][1]);
-	/*    libvlc_release(inst);
-	      inst = libvlc_new (0,NULL);
-	      gtk_widget_set_visible(player_widget, false);
-	      libvlc_media_player_release(mp);
-	      mp = libvlc_media_player_new_from_media (m);
-	      libvlc_media_player_play(mp);
-	      libvlc_media_player_set_xwindow(mp, GDK_WINDOW_XID(gtk_widget_get_window(widget)));
-	      gtk_widget_set_visible(player_widget,true);
-	      */
+void on_picture_button_clicked(GtkButton *button, gpointer user_data){
+	//libvlc_video_take_snapshot(mp,0,"/home/matheus/policorp/cam-app/src/output.png",0,0);
+	libvlc_media_player_release(mp);
+	libvlc_release(inst);
+	inst = libvlc_new(sizeof(vlc_args) / sizeof(vlc_args[0]),vlc_args);
+	m = libvlc_media_new_location(inst, "v4l2:///dev/video0");
+	const char *audio_options = ":input-slave=alsa://";  // Modify this to match your audio source
+	libvlc_media_add_option(m, audio_options);
+	const char *transcode_options = ":sout=#transcode{vcodec=theo,vb=800,fps=15,scale=1,acodec=vorb,ab=90,channels=1,samplerate=44100, filter=transform{type=hflip}}:duplicate{dst=display{noaudio},dst=std{access=file,mux=ogg,dst=screen.ogg}}";
+	libvlc_media_add_option(m, transcode_options);
+	mp = libvlc_media_player_new_from_media(m);
+	libvlc_media_release(m);
+	libvlc_media_player_play(mp);
+	libvlc_media_player_set_xwindow(mp, GDK_WINDOW_XID(gtk_widget_get_window(player_widget)));
+	gtk_widget_show_all (window);
 }
+
+
 void player_widget_on_realize(GtkWidget *widget, gpointer data) {
 	inst = libvlc_new(sizeof(vlc_args) / sizeof(vlc_args[0]), vlc_args);
 	m = libvlc_media_new_location(inst, "v4l2:///dev/video0");
 	mp = libvlc_media_player_new_from_media (m);
+	libvlc_media_release(m);
 	libvlc_media_player_play(mp);
-	libvlc_media_release (m);
 	libvlc_media_player_set_xwindow(mp, GDK_WINDOW_XID(gtk_widget_get_window(widget)));
-	g_signal_connect(G_OBJECT(widget), "configure-event", G_CALLBACK(configure_callback), NULL);    
+//	g_signal_connect(G_OBJECT(widget), "configure-event", G_CALLBACK(configure_callback), NULL);    
 	//gtk_widget_set_size_request(player_widget, cam.sizes[1][0], cam.sizes[1][1]);
 }
 static void activate (GtkApplication* app,
 		gpointer        cam_data)
 {
-
 	gtk_window_set_title (GTK_WINDOW (window), "Policorp");
 	gtk_widget_add_events(GTK_WIDGET(window), GDK_CONFIGURE);  
 	player_widget = GTK_DRAWING_AREA(gtk_builder_get_object(builder, "cam-area"));
@@ -95,11 +99,12 @@ main (int    argc,
 		char **argv)
 {   
 	int status;
+	GtkWidget *picture_button;
 	GtkApplication *app;
 	cam_compatibilites(&cam);
 	fflush(stdout);
 	gtk_init(&argc, &argv);
-	builder = gtk_builder_new_from_file("/home/kriza/Downloads/cam-app/main/data/main-window.ui");
+	builder = gtk_builder_new_from_file("/home/matheus/policorp/cam-app/data/main-window.ui");
 	GtkCssProvider *css_provider = gtk_css_provider_new();
 	GError *error = NULL;
 	if (!gtk_css_provider_load_from_path(css_provider, "style.css", &error)) {
@@ -114,6 +119,8 @@ main (int    argc,
 			GTK_STYLE_PROVIDER(css_provider),
 			GTK_STYLE_PROVIDER_PRIORITY_APPLICATION);
 
+	picture_button = GTK_WIDGET(gtk_builder_get_object(builder, "picture_button"));
+	g_signal_connect(picture_button, "clicked", G_CALLBACK(on_picture_button_clicked), NULL);
 	app = gtk_application_new("com.example.myapp", G_APPLICATION_FLAGS_NONE);
 	window = GTK_WIDGET(gtk_builder_get_object(builder, "main-window"));
 	gtk_builder_connect_signals(builder, NULL);
